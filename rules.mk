40ec94607691eababd9d23006e9b5e90f5d9cb8a
iwano <iwano@soledats-air.lan>
2023-02-06 22:03:54 -0500

    Reduce fm size

--- rules.mk
+++ rules.mk
@@ -1,6 +1,17 @@
 OLED_ENABLE = yes
 OLED_DRIVER = SSD1306   # Enables the use of OLED displays
-ENCODER_ENABLE = yes       # Enables the use of one or more encoders
 RGBLIGHT_ENABLE = yes      # Enable keyboard RGB underglow
-# CONSOLE_ENABLE = yes    # Enable debug mode
 WPM_ENABLE = yes
+LTO_ENABLE = yes
+AVR_USE_MINIMAL_PRINTF = yes
+
+
+ENCODER_ENABLE = no       # Enables the use of one or more encoders
+CONSOLE_ENABLE = no    # Enable debug mode
+COMMAND_ENABLE = no
+MOUSEKEY_ENABLE = no
+
+SPACE_CADET_ENABLE = no
+GRAVE_ESC_ENABLE = no
+MAGIC_ENABLE = no
+MUSIC_ENABLE = no
